97a948b3c0ec1225b3fee878b4669fad1d3344d5
Axel Tellez <gtacuenta47@gmail.com>
2022-06-02 20:27:03 -0500

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthLayout from "./layouts/AuthLayout";
+import ConfirmAccount from "./pages/ConfirmAccount";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import NewPassword from "./pages/NewPassword";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="registrar" element={<Register />} />
+          <Route path="olvide-password" element={<ForgotPassword />} />
+          <Route path="olvide-password/:token" element={<NewPassword />} />
+          <Route path="confirmar/:id" element={<ConfirmAccount />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/ConfirmAccount.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ConfirmAccount = () => {
+  return (
+    <div>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirma tu cuenta y Comienza a crear tus{" "}
+        <span className="text-slate-700">proyectos</span>
+      </h1>
+    </div>
+  );
+};
+
+export default ConfirmAccount;
